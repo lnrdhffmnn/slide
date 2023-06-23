ba77c7de9c5f1cc0a60554fe216f858cac66fb0e
Leonardo Hoffmann Belous <90478165+lnrdhffmnn@users.noreply.github.com>
2023-06-23 15:40:41 -0300

    Update theme

--- js/slide.js
+++ js/slide.js
@@ -1,57 +1,58 @@
 const Slide = {
-	globals: {},
+  globals: {},
 
-	init() {
-		try {
-			Slide[`slide${Index.globals.page}`]();
-		} catch {}
-	},
+  init() {
+    try {
+      Slide[`slide${Index.globals.page}`]();
+    } catch {}
+  },
 
-	slide6() {
-		const textArea = document.querySelector("textarea");
+  slide6() {
+    const textArea = document.querySelector("textarea");
 
-		/** @type {HTMLButtonElement} */
-		const execute = document.querySelector("#execute");
+    /** @type {HTMLButtonElement} */
+    const execute = document.querySelector("#execute");
 
-		/** @type {HTMLParagraphElement} */
-		const output = document.querySelector(".output");
+    /** @type {HTMLParagraphElement} */
+    const output = document.querySelector(".output");
 
-		textArea.value = 'console.log("Hello World");';
+    textArea.value = 'console.log("Hello World");';
 
-		execute.addEventListener("click", () => {
-			const log = console.log;
-			console.log = (...value) => {
-				log.apply(console, value);
-				return value;
-			};
+    execute.addEventListener("click", () => {
+      const log = console.log;
+      console.log = (...value) => {
+        log.apply(console, value);
+        return value;
+      };
 
-			output.innerHTML = eval(textArea.value);
+      output.innerHTML = eval(textArea.value);
 
-			console.log = log;
-		});
-	},
+      console.log = log;
+    });
+  },
 
-	slide7() {
-		const textArea = document.querySelector("textarea");
+  slide7() {
+    const textArea = document.querySelector("textarea");
 
-		/** @type {HTMLButtonElement} */
-		const execute = document.querySelector("#execute");
+    /** @type {HTMLButtonElement} */
+    const execute = document.querySelector("#execute");
 
-		/** @type {HTMLParagraphElement} */
-		const output = document.querySelector(".output");
+    /** @type {HTMLParagraphElement} */
+    const output = document.querySelector(".output");
 
-		textArea.value = 'for(let i=0; i <= 10; i++){ console.log(`${i} é par? ${(i & 1) == 0 ? "Sim" : "Não"}`); }';
+    textArea.value =
+      'for(let i=0; i <= 10; i++){ console.log(`${i} é par? ${(i & 1) == 0 ? "Sim" : "Não"}`); }';
 
-		execute.addEventListener("click", () => {
-			const log = console.log;
-			console.log = (...value) => {
-				log.apply(console, value);
-				return value;
-			};
+    execute.addEventListener("click", () => {
+      const log = console.log;
+      console.log = (...value) => {
+        log.apply(console, value);
+        return value;
+      };
 
-			output.innerHTML = eval(textArea.value);
+      output.innerHTML = eval(textArea.value);
 
-			console.log = log;
-		});
-	},
+      console.log = log;
+    });
+  },
 };
