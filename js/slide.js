5b50492837bfdcfc8edbd36c9574b26bd537f3b8
Leonardo <90478165+lnrdhffmnn@users.noreply.github.com>
2023-06-23 18:55:09 -0300

    Fix some style issues

--- js/slide.js
+++ js/slide.js
@@ -10,7 +10,7 @@ const Slide = {
     }
   },
 
-  slide6() {
+  executeCode() {
     /** @type {string} */
     const code = document.querySelector(".language-js").innerHTML;
 
@@ -31,55 +31,17 @@ const Slide = {
 
       console.log = log;
     });
+  },
+
+  slide6() {
+    Slide.executeCode();
   },
 
   slide7() {
-    /** @type {string} */
-    const code = document.querySelector(".language-js").innerHTML;
-
-    /** @type {HTMLButtonElement} */
-    const execute = document.querySelector("#execute");
-
-    /** @type {HTMLParagraphElement} */
-    const output = document.querySelector(".output");
-
-    execute.addEventListener("click", () => {
-      const log = console.log;
-      console.log = (...value) => {
-        log.apply(console, value);
-        return value;
-      };
-
-      output.innerHTML = eval(code);
-
-      console.log = log;
-    });
+    Slide.executeCode();
   },
 
   slide8() {
-    /** @type {string} */
-    const code = document
-      .querySelector(".language-js")
-      .innerHTML.replace(/[\n|\s]+/, "");
-
-    console.log(code);
-
-    /** @type {HTMLButtonElement} */
-    const execute = document.querySelector("#execute");
-
-    /** @type {HTMLParagraphElement} */
-    const output = document.querySelector(".output");
-
-    execute.addEventListener("click", () => {
-      const log = console.log;
-      console.log = (...value) => {
-        log.apply(console, value);
-        return value;
-      };
-
-      output.innerHTML = eval(code);
-
-      console.log = log;
-    });
+    Slide.executeCode();
   },
 };
